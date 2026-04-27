be4fef20ba8ec727bb404d0acbde2c82b4e951ba
myeonghwi <automaster5013@gmail.com>
2026-04-27 10:59:44 +0900

    React_기본_기능_다루기

--- chapter5/src/App.jsx
+++ chapter5/src/App.jsx
@@ -3,20 +3,23 @@ import Header from './component/Header'
 import Body from './component/Body'
 import Footer from './component/Footer'
 
-function App() {
-  const bodyProps = {
-    name: "이정환", 
-    location: "부천시", 
-    favorList: ["파스타", "빵", "떡볶이"], 
-  };
+function ChildComp() {
+  return <div>child component</div>;
+}
 
-  return (
+function App() {
+  // const bodyProps = {
+  //   name: "이정환", 
+  //   location: "부천시", 
+  //   favorList: ["파스타", "빵", "떡볶이"], 
+   return (
     <div className="App">
       <Header />
-      <Body {...bodyProps} />
+      <Body>
+        <ChildComp />
+      </Body> 
       <Footer />
       </div>
   );
 }
-
 export default App
--- chapter5/src/component/Body.jsx
+++ chapter5/src/component/Body.jsx
@@ -1,12 +1,10 @@
 import "./Body.css"; 
 
-function Body({name, location, favorList}) {
-    console.log(name, location, favorList);
+function Body({children}) {
+    console.log(children);
     return (
     <div className="body">
-        {name}은 {location}에 거주합니다.
-        <br />
-        {favorList.length}개의 음식을 좋아합니다. 
+        {children}
     </div>
     );
 }
