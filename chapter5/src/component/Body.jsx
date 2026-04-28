888fe84071fa3f284ea816e2f481cf491e718f94
myeonghwi <automaster5013@gmail.com>
2026-04-28 12:19:01 +0900

    [카운터]_앱만들기

--- chapter5/src/component/Body.jsx
+++ chapter5/src/component/Body.jsx
@@ -1,29 +1,29 @@
-import { useState } from "react"; 
+import { useRef, useState } from "react"; 
 import "./Body.css"; 
 
-function Viewer() {
-    console.log("viewer component update!");
-    return <div>Viewer</div>;
-}
-
 function Body() {
-    const [number, setNumber] = useState(0);
-    const onIncrease = () => {
-        setNumber (number + 1);
+    const [text, setText] = useState("");
+    const textRef = useRef();
+
+    const handleOnChange = (e) => {
+        setText(e.target.value);    // 리렌더링 ~~
     };
-    const onDecrease = () => {
-        setNumber (number - 1);
+
+    const handleOnClick = () => {
+        if (text.length < 5) {
+            textRef.current.focus();
+        }else{
+            alert(text);
+            setText("");
+        }
     };
     return (
-    <div className="body">
-        <h2>{number}</h2>
-        <Viewer />
-        <div>
-            <button onClick={onDecrease}>-</button>
-            <button onClick={onIncrease}>+</button>
+        <div className="body">
+            <input ref={textRef} value={text} onChange={handleOnChange} />
+            <button onClick={handleOnClick}>작성 완료</button>
         </div>
-    </div>
-);
+    );
 }
+
 export default Body;
 
