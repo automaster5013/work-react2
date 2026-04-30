23d30bb8e2526e680ca38a7ce755005b27179726
myeonghwi <automaster5013@gmail.com>
2026-04-30 13:05:22 +0900

    useReducer와 상태 관리

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,8 +1,10 @@
+import { useReducer } from "react";
 import "./App.css";
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
 import TodoList from "./component/TodoList";
 import { useState, useRef } from "react";
+import TestComp from "./component/TestComp";
 
 const mockTodo = [
   {
@@ -25,40 +27,68 @@ const mockTodo = [
   },
 ];
 
-function App() {
-  const [todo, setTodo] = useState(mockTodo);
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE": {
+      return [action.newItem, ...state];
+    }
+    case "UPDATE": {
+      return state.map((it) =>
+        it.id === action.targetId
+          ? {
+              ...it,
+              isDone: !it.isDone,
+            }
+          : it,
+      );
+    }
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    default:
+      return state;
+  }
+}
 
+function App() {
+  const [todo, dispatch] = useReducer(reducer, mockTodo);
   const idRef = useRef(3);
+
   const onCreate = (content) => {
-    const newItem = {
-      id: idRef.current,
-      content,
-      isDone: false,
-      createdDate: new Date().getTime(),
-    };
-    setTodo([newItem, ...todo]);
+    dispatch({
+      type: "CREATE",
+      newItem: {
+        id: idRef.current,
+        content,
+        isDone: false,
+        createdDate: new Date().getTime(),
+      },
+    });
     idRef.current += 1;
   };
 
   const onUpdate = (targetId) => {
-    setTodo(
-      todo.map((it) =>
-        it.id === targetId ? { ...it, isDone: !it.isDone } : it,
-      ),
-    );
+    dispatch({
+      type: "UPDATE",
+      targetId,
+    });
+    idRef.current += 1;
   };
 
   const onDelete = (targetId) => {
-    setTodo(todo.filter((it) => it.id !== targetId));
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
   };
 
   return (
     <div className="App">
+      <TestComp />
       <Header />
       <TodoEditor onCreate={onCreate} />
       <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
   );
 }
-
 export default App;
