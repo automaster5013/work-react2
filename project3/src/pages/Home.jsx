a61ee2bc164e81b71ad37c67e95e8101cf731608
myeonghwi <automaster5013@gmail.com>
2026-05-06 11:54:54 +0900

    [감정 일기장]_만들기

--- project3/src/pages/Home.jsx
+++ project3/src/pages/Home.jsx
@@ -1,60 +1,60 @@
-import { useContext, useEffect, useState } from "react";
-import Button from "../component/Button";
-import Header from "../component/Header";
-import { DiaryStateContext } from "../App";
-import { getMonthRangeByDate, setPageTitle } from "../util";
-import DiaryList from "../component/DiaryList";
+// import { useContext, useEffect, useState } from "react";
+// import Button from "../component/Button";
+// import Header from "../component/Header";
+// import { DiaryStateContext } from "../App";
+// import { getMonthRangeByDate, setPageTitle } from "../util";
+// import DiaryList from "../component/DiaryList";
 
 const Home = () => {
-  const data = useContext(DiaryStateContext);
+  return <div>Home 페이지입니다</div>;
+  // const data = useContext(DiaryStateContext);
 
-  const [pivotDate, setPivotDate] = useState(new Date());
-  const [filteredData, setFilteredData] = useState([]);
+  // const [pivotDate, setPivotDate] = useState(new Date());
+  // const [filteredData, setFilteredData] = useState([]);
 
-  const headerTitle = `${pivotDate.getFullYear()}년 ${
-    pivotDate.getMonth() + 1
-  }월`;
+  // const headerTitle = `${pivotDate.getFullYear()}년 ${
+  //   pivotDate.getMonth() + 1
+  // }월`;
 
-  useEffect(() => {
-    if (data.length >= 1) {
-      const { beginTimeStamp, endTimeStamp } =
-        getMonthRangeByDate(pivotDate);
-      setFilteredData(
-        data.filter(
-          (it) => beginTimeStamp <= it.date && it.date <= endTimeStamp
-        )
-      );
-    } else {
-      setFilteredData([]);
-    }
-  }, [data, pivotDate]);
+  // useEffect(() => {
+  //   if (data.length >= 1) {
+  //     const { beginTimeStamp, endTimeStamp } =
+  //       getMonthRangeByDate(pivotDate);
+  //     setFilteredData(
+  //       data.filter(
+  //         (it) => beginTimeStamp <= it.date && it.date <= endTimeStamp
+  //       )
+  //     );
+  //   } else {
+  //     setFilteredData([]);
+  //   }
+  // }, [data, pivotDate]);
 
-  useEffect(() => {
-    setPageTitle("Winterlood의 감정 일기장");
-  }, []);
+  // useEffect(() => {
+  //   setPageTitle("Winterlood의 감정 일기장");
+  // }, []);
 
-  const onIncreaseMonth = () => {
-    setPivotDate(
-      new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1)
-    );
-  };
+  // const onIncreaseMonth = () => {
+  //   setPivotDate(
+  //     new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1)
+  //   );
+  // };
 
-  const onDecreaseMonth = () => {
-    setPivotDate(
-      new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1)
-    );
-  };
+  // const onDecreaseMonth = () => {
+  //   setPivotDate(
+  //     new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1)
+  //   );
+  // };
 
-  return (
-    <div>
-      <Header
-        title={headerTitle}
-        leftChild={<Button text={"<"} onClick={onDecreaseMonth} />}
-        rightChild={<Button text={">"} onClick={onIncreaseMonth} />}
-      />
-      <DiaryList data={filteredData} />
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <Header
+  //       title={headerTitle}
+  //       leftChild={<Button text={"<"} onClick={onDecreaseMonth} />}
+  //       rightChild={<Button text={">"} onClick={onIncreaseMonth} />}
+  //     />
+  //     <DiaryList data={filteredData} />
+  //   </div>
+  // );
 };
-
 export default Home;
